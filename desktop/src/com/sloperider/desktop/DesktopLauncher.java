0eb6f4e8a05083e2b0fdec449780eef5f481c624
Julien Cloute <julien.cloute@gmail.com>
2015-11-07 20:07:21 +0100

    Added libgdx project base.

---
+++ desktop/src/com/sloperider/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sloperider.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sloperider.SlopeRider;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SlopeRider(), config);
+	}
+}
