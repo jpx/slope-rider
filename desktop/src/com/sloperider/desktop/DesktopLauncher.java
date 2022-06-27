40ae16f3d0450b181f7b58d3123acd919d3d0758
Julien Clouté <julien.cloute@gmail.com>
2022-06-27 16:19:21 +0200

    Fixed the desktop launcher.

--- desktop/src/com/sloperider/desktop/DesktopLauncher.java
+++ desktop/src/com/sloperider/desktop/DesktopLauncher.java
@@ -1,14 +1,14 @@
 package com.sloperider.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.sloperider.SlopeRider;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1280;
-        config.height = 720;
-		new LwjglApplication(new SlopeRider(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Slope Rider");
+		new Lwjgl3Application(new SlopeRider(), config);
 	}
 }
