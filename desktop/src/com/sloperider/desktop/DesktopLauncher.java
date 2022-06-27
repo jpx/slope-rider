9391f59bc3338b275d786ee6e68ef7e80e1b2788
Julien Clouté <julien.cloute@gmail.com>
2022-06-28 01:01:58 +0200

    Restored desktop window size to 720p.

--- desktop/src/com/sloperider/desktop/DesktopLauncher.java
+++ desktop/src/com/sloperider/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Slope Rider");
-		config.setWindowedMode(1920, 1080);
+		config.setWindowedMode(1280, 720);
 		new Lwjgl3Application(new SlopeRider(), config);
 	}
 }
