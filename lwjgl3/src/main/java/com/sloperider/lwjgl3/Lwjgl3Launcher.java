f9306a975d6ae6b45e52a8fb61cb833203522343
jpx <julien.cloute@gmail.com>
2024-09-12 19:23:00 +0200

    Added spine runtime Box2DExample.

--- lwjgl3/src/main/java/com/sloperider/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/com/sloperider/lwjgl3/Lwjgl3Launcher.java
@@ -2,6 +2,7 @@ package com.sloperider.lwjgl3;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.sloperider.Box2DExample;
 import com.sloperider.Configuration;
 import com.sloperider.SlopeRider;
 
@@ -15,17 +16,18 @@ public class Lwjgl3Launcher {
     private static Lwjgl3Application createApplication(String[] args) {
         final String startingLevel = args.length > 0 ? args[0] : null;
 
-        return new Lwjgl3Application(new SlopeRider().configuration(new Configuration() {
-            @Override
-            public boolean debug() {
-                return true;
-            }
-
-            @Override
-            public String startingLevel() {
-                return startingLevel;
-            }
-        }), getDefaultConfiguration());
+        return new Lwjgl3Application(new Box2DExample());
+//        return new Lwjgl3Application(new SlopeRider().configuration(new Configuration() {
+//            @Override
+//            public boolean debug() {
+//                return true;
+//            }
+//
+//            @Override
+//            public String startingLevel() {
+//                return startingLevel;
+//            }
+//        }), getDefaultConfiguration());
     }
 
     private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
